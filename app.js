17c49bf08e12a8f131778caecd4486c6776eb8f2
Harshil Bulsara <bulsaraharshil@gmail.com>
2020-12-06 20:28:43 +0530

    feat: added Shakespeare talk

---
+++ app.js
@@ -0,0 +1,28 @@
+var btnTranslate = document.querySelector("#btn-translate")
+var txtInput = document.querySelector("#txt-input")
+var outputDiv = document.querySelector("#output")
+
+var serverUrl = "https://api.funtranslations.com/translate/shakespeare.json"
+
+function getTranslationURL(input) {
+    return serverUrl + "?" + "text=" + input
+}
+
+function errorHandler(e) {
+    console.log("Error " + e)
+    alert("Error occured, please try after sometime")
+}
+
+function clickHandler() {
+    var inputTxt = txtInput.value
+    fetch(getTranslationURL(inputTxt))
+        .then(response => response.json())
+        .then(json => {
+                var translatedText = json.contents.translated;
+                outputDiv.innerText = translatedText
+            }
+        )
+        .catch(errorHandler)
+}
+
+btnTranslate.addEventListener("click", clickHandler)
